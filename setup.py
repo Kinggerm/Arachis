71c609a6bd3eefe4c66955e03efd9b9f251b0c7f
Kinggerm <cndslr@foxmail.com>
2018-03-27 10:34:56 +0800

    update

--- setup.py
+++ setup.py
@@ -3,14 +3,14 @@ import os
 
 setup(
       name="arachis",
-      version="0.40",
+      version="0.41",
       description="Analyzing ReArrangements with Circular Headed Incomplete Sequences",
       author="Jianjun Jin",
       author_email='jinjianjun@mail.kib.ac.cn',
       url="http://github???",
       license="GNU General Public License, version 3",
       packages=["arachis"],
-      scripts=["scripts/run_pypmag.py", "scripts/compare_grimm_files.py"],
+      scripts=["scripts/run_pypmag.py", "scripts/compare_grimm_files.py", "scripts/map_tree_labels.py"],
       )
 
 os.system('rm -vrf ./build ./dist ./*.pyc ./*.tgz ./*.egg-info')
